187e58853d8207a339f8bf8e852cde3d3125fb06
Damgom <wisewish1@gmail.com>
2023-02-15 00:32:12 +0900

    INIT: mysql remind

---
+++ SQL/mysql-remind/src/main/java/com/databasetutorial/mysqlremind/MysqlRemindApplication.java
@@ -0,0 +1,13 @@
+package com.databasetutorial.mysqlremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlRemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlRemindApplication.class, args);
+	}
+
+}
